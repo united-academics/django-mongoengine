a1c145e9a74527fcd09d5b918dbe2d9bafc966c6
Sergiu <sergiu.zaharie@united-academics.org>
2017-06-28 20:11:15 +0300

    subclass AllValuesFieldListFilter to use mongo distinct

---
+++ django_mongoengine/mongo_admin/filters.py
@@ -0,0 +1,22 @@
+from django.contrib.admin import filters
+from django.contrib.admin.utils import reverse_field_path
+
+
+class AllValuesFieldListFilter(filters.AllValuesFieldListFilter):
+    def __init__(self, field, request, params, model, model_admin, field_path):
+        self.lookup_kwarg = field_path
+        self.lookup_kwarg_isnull = '%s__isnull' % field_path
+        self.lookup_val = request.GET.get(self.lookup_kwarg)
+        self.lookup_val_isnull = request.GET.get(self.lookup_kwarg_isnull)
+        self.empty_value_display = model_admin.get_empty_value_display()
+        parent_model, reverse_path = reverse_field_path(model, field_path)
+        # Obey parent ModelAdmin queryset when deciding which options to show
+        if model == parent_model:
+            queryset = model_admin.get_queryset(request)
+        else:
+            queryset = parent_model._default_manager.all()
+        self.lookup_choices = (queryset
+                               .order_by(field.name)
+                               .distinct(field.name))
+        super(AllValuesFieldListFilter, self).__init__(
+            field, request, params, model, model_admin, field_path)
